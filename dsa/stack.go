d8a24b1218080d545b50d8cbe80d49584c85ca4c
cool-pants <muban.sg@gmail.com>
2024-08-05 15:10:03 +0530

    feat: changes

--- dsa/stack.go
+++ dsa/stack.go
@@ -1,21 +1,19 @@
 package dsa
 
-
 type Stack[T any] []T
 
-
-func (s *Stack[T]) Pop() T{
-    arr := *s
-    val := arr[0]
-    *s = arr[1 : len(arr)]
-    return val
+func (s *Stack[T]) Pop() T {
+	arr := *s
+	val := arr[0]
+	*s = arr[1:]
+	return val
 }
 
 func (s *Stack[T]) Peek() T {
-    arr := *s
-    return arr[0]
+	arr := *s
+	return arr[0]
 }
 
 func (s *Stack[T]) Push(x T) {
-    *s = append(*s, x)
+	*s = append(*s, x)
 }
